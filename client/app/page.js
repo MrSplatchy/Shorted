011ff96bf5556dc7d2bd5a4741862df2ec5de786
MrSplatchy <mrsplatchy@proton.me>
2026-07-05 22:37:32 +0200

    feat: began frontend developpement

---
+++ client/app/page.js
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <main className="flex flex-col flex-8  font-sans bg-dots items-center justify-center" >
+        <h1 className=" font-semibold "> Hi </h1>
+      </main>
+
+      <hr/>
+      
+      <footer className="flex-2 relative">footer</footer>
+    </div>
+
+  );
+}
