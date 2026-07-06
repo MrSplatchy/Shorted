a002ea3e398e157e75f286e952d21abf9d4f0e4a
MrSplatchy <mrsplatchy@proton.me>
2026-07-06 15:16:40 +0200

    Maked it more *stylish*

--- client/app/page.js
+++ client/app/page.js
@@ -2,12 +2,14 @@ export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex flex-col flex-8  font-sans bg-dots items-center justify-center" >
-        <h1 className=" font-semibold "> Hi </h1>
+        <h1 className=" font-semibold text-4xl text-center ">Make your urls shorter with Shorted</h1>
       </main>
 
-      <hr/>
+      <hr className=" text-emerald-100"/>
       
-      <footer className="flex-2 relative">footer</footer>
+      <footer className="flex-1 bg-neutral-800">
+        <p className="font-mono opacity-45 text-center ">made by Mr.<a href="https://github.com/MrSplatchy" target="__blank" className=" underline">Splatchy</a></p>
+      </footer>
     </div>
 
   );
